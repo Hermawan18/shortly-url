c26ca18081081cb9a57c3aada8af2131b957d3bd
Hermawan18 <hermawan18.hty@gmail.com>
2024-05-14 14:31:31 +0700

    fix: update icon

--- shortly/src/app/layout.tsx
+++ shortly/src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
+import Icon from '@/app/assets/images/favicon-32x32.png';
 import './globals.css';
 
 const poppins = Poppins({ weight: ['500', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: '/assets/images/favicon-32x32.png',
+  },
   title: 'Shortly',
   description: 'Shorten url',
 };
